285483cf37b989f0fcc74eccd2b3d299811bf076
amania-Gostd <78249200+amania-Gostd@users.noreply.github.com>
2021-02-14 08:17:08 +0900

    Add files via upload

--- update.py
+++ update.py
@@ -9,30 +9,36 @@ application = True
 Unsignedapplication = false
 nostoreapp = false
 appcmd = None
-Ver = {v}'''
+Ver = {v}
+AUP = True'''
     return i
 def install(app):
-    if app == "new":
-        s = open("new.aup","r",encoding="utf-8")
-        s = s.read()
-        d = open("DOS.py","w",encoding="utf-8")
-        d.write(s)
-        d.close()
-    else:
-        try:
+    try:
+        if app == "new":
+            d = open("new.aup","r",encoding="utf-8")
+            d = d.read()
+            s = open("DOS.py","w",encoding="utf-8")
+            s.write(d)
+            s.close()
+            s = open("config.ini","w",encoding="utf-8")
+            s.write(ini(aup["Defalt"]["New-Ver"]))
+            s.close()
+        else:
             d = config["Defalt"][app]
             urllib.request.urlretrieve(d, "DOS.py")
             a = ini(app)
-            s = open("config.ini","w")
+            s = open("config.ini","w",encoding="utf-8")
             s.write(a)
             s.close()
-        except KeyError as e:
-            print(f"{app}は存在しません")
+    except KeyError as e:
+        print(f"{app}は存在しません")
 print("使用可能なAUIFを検索しています")
 urllib.request.urlretrieve("https://raw.githubusercontent.com/amania-Gostd/Gostd/main/update.ini", "update.ini")
 config = configparser.ConfigParser()
+aup = configparser.ConfigParser()
 ###config.read('update.ini')
 config.readfp(codecs.open("update.ini", "r", "utf8"))
+aup.readfp(codecs.open("aup.ini", "r", "utf8"))
 print("ダウンロードするバージョンを選択してください(Ver.10.2以上のみ使用できます)")
 for key in config['Defalt']:
     print(key)
